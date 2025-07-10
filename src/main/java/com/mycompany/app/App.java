066faea3d18e0fd1e7d3d668e816437ede1cafd6
Sheenu <singsachin348@gmail.com>
2025-07-10 10:08:28 +0530

    file updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+	Thread.sleep(100000);
     }
 
     private final String getMessage() {
