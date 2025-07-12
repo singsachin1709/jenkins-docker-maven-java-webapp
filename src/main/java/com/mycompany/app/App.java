390c74de64348161175df1333659774ac875f787
Sheenu <singsachin348@gmail.com>
2025-07-12 21:06:53 +0530

    file updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,10 @@ public class App
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(new App().getMessage());
+	if (args == null || args.length == 0 || !"test".equals(args[0])) {
 	Thread.sleep(100000);
+
+	}
     }
 
     private final String getMessage() {
